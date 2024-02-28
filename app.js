9301ac0a2f8d6e2bb31bf97830bb6e63fc42774a
Jessica Luz <jessica.luz@vodafone.com>
2024-02-28 22:52:51 +0000

    Using types from Typescript to ensure that the function receives exatcly the type of parameter it's expecting

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Learning TypeScript!");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 3.5;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
 console.log("Learning TypeScript!")
+
+function add(n1, n2){
+    return n1+n2;
+}
+
+const number1 = 5;
+const number2 = 3.5;
+
+const result = add(number1,number2)
+
+console.log(result);
