94851ce22024421d8e6269617bd843d78630e2cc
Jessica Luz <jessica.luz@vodafone.com>
2024-02-29 16:14:52 +0000

    Working with other different types in Typescript, boolean and string.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 console.log("Learning TypeScript!");
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, phrase, showResult) {
+    var result = n1 + n2;
+    if (result) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 3.5;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is: ";
+add(number1, number2, resultPhrase, printResult);
--- app.ts
+++ app.ts
@@ -1,12 +1,17 @@
 console.log("Learning TypeScript!")
 
-function add(n1, n2){
-    return n1+n2;
+function add(n1: number, n2: number, phrase: string, showResult: boolean){
+    const result = n1+n2;
+    if(result){
+        console.log(resultPhrase + result);    
+    }else {
+        return n1+n2;
+    }
 }
 
 const number1 = 5;
 const number2 = 3.5;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-const result = add(number1,number2)
-
-console.log(result);
+add(number1,number2,resultPhrase, printResult);
