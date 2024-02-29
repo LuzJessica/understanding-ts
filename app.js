348eaead9e095f4b9d77fec21a0c6d3dd7cd317d
Jessica Luz <jessica.luz@vodafone.com>
2024-02-29 16:48:15 +0000

    Working with Objects

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-console.log("Learning TypeScript!");
-function add(n1, n2, phrase, showResult) {
-    var result = n1 + n2;
-    if (result) {
-        console.log(resultPhrase + result);
-    }
-    else {
-        return n1 + n2;
-    }
-}
-var number1 = 5;
-var number2 = 3.5;
-var printResult = true;
-var resultPhrase = "Result is: ";
-add(number1, number2, resultPhrase, printResult);
+var person = {
+    name: 'Jéssica',
+    age: '31'
+};
+console.log(person.name);
--- app.ts
+++ app.ts
@@ -1,17 +1,6 @@
-console.log("Learning TypeScript!")
+const person = {
+    name: 'Jéssica',
+    age: '31'
+};
 
-function add(n1: number, n2: number, phrase: string, showResult: boolean){
-    const result = n1+n2;
-    if(result){
-        console.log(resultPhrase + result);    
-    }else {
-        return n1+n2;
-    }
-}
-
-const number1 = 5;
-const number2 = 3.5;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(number1,number2,resultPhrase, printResult);
+console.log(person.name);
