2d32248f4cf234527f5b761e6c93bf0cabde454c
Jessica Luz <jessica.luz@vodafone.com>
2024-03-04 14:37:04 +0000

    Working with literal types

--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
-function combine(input1: number | string, input2: number | string){
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-string'){
     let result;
-    if(typeof input1 === 'number' && typeof input2 === 'number'){
-        result = input1 + input2;
+    if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number'){
+        result = +input1 + +input2;
     }else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
 
-const combinedAges = combine(31,30);
+const combinedAges = combine(31,30,'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine ('Jéssica','Matheus');
+const combinedNames = combine('Jéssica','Matheus','as-string');
 console.log(combinedNames);
+
+const combinedStringAges = combine('31','30','as-number');
+console.log(combinedStringAges);
