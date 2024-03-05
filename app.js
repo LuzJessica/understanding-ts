13978e1e0139af53634bd34845e406471e75052c
Jessica Luz <jessica.luz@vodafone.com>
2024-03-05 10:22:07 +0000

    Working with callbacks

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+function addString(str) {
+    return str;
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 var combinedValues;
 combinedValues = add;
+combinedValues = addString; /* It returns error because the function
+addString doesn't match the function defined for the variable
+combinedValue. It has only one parameter and it's a string. And the variable
+expect 2 parameters and both should be numbers*/
 console.log(combinedValues(9, 8));
+addAndHandle(20, 20, function (result) {
+    console.log(result);
+    return result;
+});
--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function addString (str: string){
     return str;
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) =>  void){
+    const result = n1 + n2;
+    cb(result);
+}
+
 let combinedValues: (a: number, b: number) => number;
 
 combinedValues = add;
@@ -15,3 +20,8 @@ combinedValue. It has only one parameter and it's a string. And the variable
 expect 2 parameters and both should be numbers*/
 
 console.log(combinedValues(9,8));
+
+addAndHandle(20, 20, (result) => {
+    console.log(result);
+    return result;
+})
