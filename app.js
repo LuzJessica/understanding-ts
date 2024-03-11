06144288bd9d0a1d3b5f812bee18d1e279b0e20e
Jessica Luz <jessica.luz@vodafone.com>
2024-03-11 15:30:26 +0000

    Adding more attributes and methods. Working with private access modifiers

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 "use strict";
 class Department {
-    constructor(n) {
+    constructor(n, d) {
         this.name = n;
+        this.description = d;
+        this.employees = [];
+    }
+    describe() {
+        console.log('Department ' + this.name + ' - ' + this.description);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+        console.log('Employee ' + employee + ' added successfully to the ' + this.name + ' department.');
+    }
+    countEmployee() {
+        console.log('We have now ' + this.employees.length + ' employees in the ' + this.name + ' department.');
+    }
+    listOfEmployees() {
+        console.log('Here are the names of our employees in the ' + this.name + ' department: ' + this.employees);
     }
 }
-const hr = new Department('Human Resources');
-console.log(hr);
+const hr = new Department('IT', 'Information Technology');
+hr.describe();
+hr.addEmployee('Jéssica');
+hr.addEmployee('Matheus');
+hr.addEmployee('P2');
+hr.countEmployee();
+hr.listOfEmployees();
--- app.ts
+++ app.ts
@@ -1,10 +1,36 @@
 class Department {
     name: string;
+    description: string;
+    employees: string[];
 
-    constructor (n: string){
+    constructor (n: string, d: string){
         this.name = n;
+        this.description = d;
+        this.employees = []
+    }
+
+    describe(){
+        console.log('Department ' + this.name + ' - ' + this.description);
+    }
+
+    addEmployee(employee:string){
+        this.employees.push(employee);
+        console.log('Employee ' + employee + ' added successfully to the ' + this.name + ' department.');
+    }
+
+    countEmployee(){
+        console.log('We have now ' + this.employees.length + ' employees in the ' + this.name + ' department.');
+    }
+
+    listOfEmployees(){
+        console.log('Here are the names of our employees in the ' + this.name + ' department: '  + this.employees);
     }
 }
+const hr = new Department('IT', 'Information Technology');
 
-const hr = new Department('Human Resources');
-console.log(hr);
+hr.describe();
+hr.addEmployee('Jéssica');
+hr.addEmployee('Matheus');
+hr.addEmployee('P2');
+hr.countEmployee();
+hr.listOfEmployees();
