9bd25b6aae1e952de1162ba1c0ea0b9d05a2fcad
Jessica Luz <jessica.luz@vodafone.com>
2024-03-11 14:17:04 +0000

    Starting module about classes and Interfaces. 1 item: Creating classes and instances of classes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict";
-const states = ['MG', 'SP', 'RJ', 'DF', 'MT', 'PR'];
-const [minasGerais, saoPaulo, rioDeJaneiro, distritoFederal, matoGrosso, parana] = states;
-console.log(minasGerais);
-/*The array object  destructuring is usefull when you want to
-insert the values of the array/objects in another variable. In cases where
-the array/object has few items it seems not to be worth but when you
-have an array with many items it is */
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
+}
+const hr = new Department('Human Resources');
+console.log(hr);
--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
-const states = ['MG','SP','RJ','DF','MT','PR'];
+class Department {
+    name: string;
 
-const [minasGerais, saoPaulo , rioDeJaneiro, distritoFederal, matoGrosso ,parana] = states;
+    constructor (n: string){
+        this.name = n;
+    }
+}
 
-console.log(minasGerais);
-
-/*The array object  destructuring is usefull when you want to 
-insert the values of the array/objects in another variable. In cases where
-the array/object has few items it seems not to be worth but when you 
-have an array with many items it is */
-
+const hr = new Department('Human Resources');
+console.log(hr);
