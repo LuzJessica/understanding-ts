f74e7388f04fe5414fcaefa53dfc180ad74c63e5
Jessica Luz <jessica.luz@vodafone.com>
2024-03-14 17:29:25 +0000

    Type guards for types, custom types and classes

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 "use strict";
+const printEmployeeInformation = (emp) => {
+    console.log('Name: ' + emp.name); // works because both Admin and Employee types have property name
+    if ('privilegies' in emp) { //type guard to validate if its Admin type
+        console.log('Privileges: ' + emp.privilegies);
+    }
+    if ('startDate' in emp) { //or if it is Employee type
+        console.log('Start Data: ' + emp.startDate);
+    }
+};
 let user;
 user = {
     name: 'Jéssica',
     privileges: ['Create server'],
     startDate: new Date()
 };
+printEmployeeInformation(user);
+const add = (a, b) => {
+    if (typeof a === 'string' || typeof b === 'string') { //typeof here is used as type guard and it can be used for types like object, string, number, boolean
+        return a.toString() + b.toString();
+    }
+    return a + b;
+};
+class Car {
+    drive() {
+        console.log('I am driving...');
+    }
+}
+class Truck {
+    drive() {
+        console.log('I am driving a truck...');
+    }
+    loadCargo() {
+        console.log('I am loading cargo');
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+const useVehicle = (vehicle) => {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo;
+    }
+};
+useVehicle(v1);
+useVehicle(v2);
--- app.ts
+++ app.ts
@@ -8,8 +8,19 @@ type Employee = {
     startDate: Date;
 }
 
-type ElevatedEmployee = Admin & Employee;
+type UnknowEmployee = Admin | Employee;
 
+const printEmployeeInformation = (emp: UnknowEmployee) => {
+    console.log('Name: ' + emp.name);// works because both Admin and Employee types have property name
+    if('privilegies' in emp){//type guard to validate if its Admin type
+        console.log('Privileges: ' + emp.privilegies);
+    }
+    if('startDate' in emp){//or if it is Employee type
+        console.log('Start Data: ' + emp.startDate);
+    }
+}
+
+type ElevatedEmployee = Admin & Employee;
 let user: ElevatedEmployee;
 
 user = {
@@ -18,6 +29,8 @@ user = {
     startDate: new Date()
 };
 
+printEmployeeInformation(user);
+
 //We can combine all types, for example:
 
 type Combinable = string | number;
@@ -25,5 +38,44 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+const add = (a: Combinable, b: Combinable) => {
+    if(typeof a === 'string' || typeof b === 'string'){ //typeof here is used as type guard and it can be used for types like object, string, number, boolean
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+
+
+class Car{
+    drive(){
+        console.log('I am driving...');
+    }
+}
+
+class Truck{
+    drive(){
+        console.log('I am driving a truck...');
+    }
+
+    loadCargo(){
+        console.log('I am loading cargo');
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle:Vehicle) => {
+    vehicle.drive();
+    if(vehicle instanceof Truck){
+        vehicle.loadCargo;
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
 
     
