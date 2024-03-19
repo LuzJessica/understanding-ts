ecb3e32fa1598a3752db3ece93b1ab85471cf7fc
Jessica Luz <jessica.luz@vodafone.com>
2024-03-19 21:50:50 +0000

    Creating and using autobind decorator

--- DragAndDrop/src/app.ts
+++ DragAndDrop/src/app.ts
@@ -1,7 +1,29 @@
+//Bind decorator
+function autobind(
+    _: any, // the _ meand that I know that this property will not be used but I need to have as parameter
+    _2: string,
+    descriptor: PropertyDescriptor
+){
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get(){
+        const boundFunc = originalMethod.bind(this);
+        return boundFunc;
+    }
+  };
+  return adjDescriptor; 
+}
+
+//Project Input Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
+
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
 
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +31,26 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = 'user-input';
+
+        this.titleInput = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInput = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInput = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
         this.attach();
+}
+
+@autobind
+private submitHandler(event: Event){
+    event.preventDefault();
+    console.log(this.titleInput.value);
+    console.log(this.descriptionInput.value);
+    console.log(this.peopleInput.value);
+}
+
+private configure(){
+    this.element.addEventListener('submit', this.submitHandler)
 }
 
 private attach(){
