c313ef79adf600ee801e634557421384102a8dec
Jessica Luz <jessica.luz@vodafone.com>
2024-03-19 14:27:41 +0000

    Decorators in properties

--- app.js
+++ app.js
@@ -5,31 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(logString) {
-    return function (constructor) {
-        console.log(logString);
-        console.log(constructor);
-    };
+function Log(target, propertyName) {
+    console.log('Property decorator!');
+    console.log(target, propertyName);
 }
-function WithTemplate(template, hookId) {
-    return function (constructor) {
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1').textContent = p.name;
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
         }
-    };
-}
-//@Logger('LOGGING - PERSON') 
-let Person = class Person {
-    constructor() {
-        this.name = 'Max';
-        console.log('Creating person object...');
+        else {
+            throw new Error('Invalid price');
+        }
     }
-};
-Person = __decorate([
-    WithTemplate('<h1>My Person Object</h1>', 'app')
-], Person);
-const pers = new Person();
-console.log(pers);
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
--- app.ts
+++ app.ts
@@ -1,30 +1,27 @@
-function Logger(logString: string){
-    return function(constructor: Function){
-        console.log(logString);
-        console.log(constructor);
-    }
+function Log(target: any, propertyName: string | Symbol){
+    console.log('Property decorator!');
+    console.log(target,propertyName);
 }
 
-function WithTemplate(template: string, hookId: string){
-    return function(constructor:any){
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if(hookEl){
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
+class Product{
+    @Log
+    title: string;
+    private _price: number;
+
+    set price(val: number){
+        if(val>0){
+            this._price = val;
+        }else{
+            throw new Error('Invalid price');
         }
     }
-}
 
-//@Logger('LOGGING - PERSON') 
-@WithTemplate('<h1>My Person Object</h1>', 'app')
-class Person{
-    name = 'Max';
-    constructor(){
-        console.log('Creating person object...');
+    constructor(t: string, p:number){
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number){
+        return this._price * (1 + tax);
     }
 }
-
-const pers =  new Person();
-console.log(pers);
-
