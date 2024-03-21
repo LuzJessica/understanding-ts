1e24852ca859e2263d671e8d551691bb9775871d
Jessica Luz <jessica.luz@vodafone.com>
2024-03-21 09:56:44 +0000

    rendering lists

--- DragAndDrop/src/app.ts
+++ DragAndDrop/src/app.ts
@@ -56,6 +56,43 @@ function autobind(
   return adjDescriptor; 
 }
 
+//Project List Class - Rendered list in a certain place of application
+class ProjectList{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement; //element which will host other elements inside of it, in this case, the app div
+    element: HTMLElement; // the element which will be hosted, in this case the list, in this case, the section element
+
+    constructor(private type: 'active' | 'finished' | 'approved' | 'reproved' | 'toreview' | 'inanalysis'){
+        this.templateElement = document.getElementById('project-list')! as  HTMLTemplateElement;//getting access to template
+        this.hostElement = document.getElementById('app')! as HTMLDivElement; //getting access to the div 
+
+        const importedNode = document.importNode(this.templateElement.content,true);//importing element 
+        this.element = importedNode.firstElementChild as HTMLElement;//storing element (section)
+        this.element.id = `${this.type}-projects`; //the id here is dynamic because we will have more than one list of projects
+        this.attach();
+        this.renderContent();  
+    }
+
+    private renderContent(){//for each instance of the class will set id and H2 title for each list
+        const listId = `${this.type}-projects-list`;//to inject dynamic content
+        this.element.querySelector('ul')!.id = listId;// each list will have its own id with name type + -projects-list, for example active-projects-list
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS ';// list title   
+
+    }
+
+    /**
+     This function attach the element inside the host element using as reference the position
+     beforebegin, before target element, in this case the target element is div, so it would be before it
+     afterbegin, just inside the target element, in this case the div
+     beforeend, just inside the target element, after its last child.
+     afterend, after the target element ifself
+     */
+    private attach(){
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
+
 //Project Input Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
@@ -79,8 +116,6 @@ class ProjectInput {
         this.peopleInput = this.element.querySelector('#people') as HTMLInputElement;
         this.configure();
         this.attach();
-
-        
     }
 
 private gatherUserInput(): [string, string, number] | void{ // return tuple or void
@@ -143,3 +178,9 @@ private attach(){
     }
 }
 const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
+// const approvedProjectList = new ProjectList('approved');
+// const reprovedProjectList = new ProjectList('reproved');
+// const toReviewProjectList = new ProjectList('toreview');
+// const inAnalysisProjectList = new ProjectList('inanalysis');
